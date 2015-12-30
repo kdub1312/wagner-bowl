c8c9c66f5072de03785670ed85ab89acda5adfd5
kdub1312 <kwagner1312@gmail.com>
2015-12-30 16:52:45 -0600

    Added ngRoute Logic
    Split controller logic and app to manage controllers into separate
    files. Included angular-route.min.js and added html partial

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,7 @@
-var wagnerBowlApp = angular.module('wagnerBowlApp', []);
-    
-wagnerBowlApp.controller('WagnerBowlController', ['$scope', '$http', function WagnerBowlController($scope, $http) {
+var competitorController = angular.module('competitorController', []);
+/*var wagnerBowlApp = angular.module('wagnerBowlApp', []);*/
+
+competitorController.controller('CompetitorListController', ['$scope', '$http', function WagnerBowlController($scope, $http) {
         
 $http.get('js/data.json').success(function(data) {
     $scope.competitors = data;
