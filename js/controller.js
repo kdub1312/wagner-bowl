ea21707b03a05754dff84cd8c724bef6f22d3172
kdub1312 <kwagner1312@gmail.com>
2015-12-30 14:33:15 -0600

    Update to index.html and controller.js
    I've updated index.html and controller.js. Index.html to use ng-repeat
    and index.html changed the 'controller' property to 'controllers

--- js/controller.js
+++ js/controller.js
@@ -3,7 +3,7 @@ var wagnerBowlApp = angular.module('wagnerBowlApp', []);
 wagnerBowlApp.controller('WagnerBowlController', function WagnerBowlController($scope, $http) {
         
 $http.get('js/data.json').success(function(data) {
-    $scope.competitor = data;
+    $scope.competitors = data;
 }); 
             
     });
